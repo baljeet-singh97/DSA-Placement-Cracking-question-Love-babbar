1ff78c99d364f809596a91b24e69978d57bf53f1
Baljeet Singh <prince.singh4000@gmail.com>
2021-06-13 21:23:47 +0530

    Create Merge Intervals.cpp

---
+++ Array/Merge Intervals.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& v) 
+    {
+        sort(v.begin(),v.end());
+        
+        vector<vector<int>>ans;
+        
+        int start=v[0][0];
+        int end = v[0][1];
+        
+        for(int i=1; i<v.size(); i++)
+        {
+            if(v[i][0]>=start && v[i][0]<=end)
+            {
+                end=max(end,v[i][1]);
+            }
+            else
+            {
+                ans.push_back({start,end});
+                start = v[i][0];
+                end = v[i][1];
+            }
+        }
+        ans.push_back({start,end});
+        return ans;
+        
+    }
+};
+
