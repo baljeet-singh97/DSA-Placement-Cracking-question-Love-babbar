96ce9dd53bbf180fe48d56869d78904ca48be379
Baljeet Singh <prince.singh4000@gmail.com>
2021-06-26 16:44:23 +0530

    Create Rotate String.cpp

---
+++ string/Rotate String.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal)
+    {
+        if(s.length()!=goal.length())
+        return false;
+        string tmp = s+s;
+        return(tmp.find(goal)!=string::npos);
+        
+    }
+};
